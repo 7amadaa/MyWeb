c8e7bf4fdcd7ad5db949c65396c5c7499001a263
7amadaa <62021904+7amadaa@users.noreply.github.com>
2025-07-30 20:06:16 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Configuration for JSONBin.io storage
 const CONFIG = {
     JSONBIN_API_KEY: '$2a$10$8PZ2/XKleazeAypDa7P6zukUR/Pk2Eay07guUQAWc1aJwbfk28Ij2',
-    JSONBIN_BIN_ID: '688a35567b4b8670d8a9e718',
+    JSONBIN_BIN_ID: '688a2dacae596e708fbe4d60',
     JSONBIN_URL: 'https://api.jsonbin.io/v3/b'
 };
 
